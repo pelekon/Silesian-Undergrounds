94d0558b2b03be9624fdf488cefd9f9e1add976a
Pelekon <pelekon@gmail.com>
2019-03-15 15:28:53 +0100

    Add first homework

---
+++ PdOne/PdOne/Game1.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using PdOne.Misc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PdOne
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        DrawableObject player;
+        List<DrawableObject> objects = new List<DrawableObject>();
+        SpriteFont font;
+        
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            Texture2D holder;
+            holder = Content.Load<Texture2D>("one");
+            player = new DrawableObject(holder, 0, 0, 1f);
+
+            holder = Content.Load<Texture2D>("two");
+            DrawableObject obj = new DrawableObject(holder, 200, 200, 2f);
+            objects.Add(obj);
+
+            font = Content.Load<SpriteFont>("File");
+        }
+
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            float forceX = 0f;
+            float forceY = 0f;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.D) || Keyboard.GetState().IsKeyDown(Keys.Right))
+                forceX = 1f;
+            if (Keyboard.GetState().IsKeyDown(Keys.A) || Keyboard.GetState().IsKeyDown(Keys.Left))
+                forceX = -1f;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.W) || Keyboard.GetState().IsKeyDown(Keys.Up))
+                forceY = -1f;
+            if (Keyboard.GetState().IsKeyDown(Keys.S) || Keyboard.GetState().IsKeyDown(Keys.Down))
+                forceY = 1f;
+
+            player.AddForce(forceX, forceY);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            DrawGameObjects();
+            spriteBatch.End();
+
+
+            base.Draw(gameTime);
+        }
+
+        private void DrawGameObjects()
+        {
+            spriteBatch.Draw(player.GetTexture(), player.GetPosition(), scale: player.GetScaleVector());
+
+            foreach (var obj in objects)
+                spriteBatch.Draw(obj.GetTexture(), obj.GetPosition(), scale: obj.GetScaleVector());
+
+            if (CheckCollision())
+                spriteBatch.DrawString(font, "There is collision", new Vector2(300, 0), Color.Red);
+            else
+                spriteBatch.DrawString(font, "There is no collision", new Vector2(300, 0), Color.Yellow);
+        }
+
+        private bool CheckCollision()
+        {
+            bool found = false;
+            foreach (var obj in objects)
+            {
+                if (player.CheckForColision(obj))
+                    found = true;
+            }
+
+            return found;
+        }
+    }
+}
---
+++ PdOne/PdOne/Misc/DrawableObject.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace PdOne.Misc
+{
+    public class DrawableObject
+    {
+        private Texture2D texture;
+
+        private float posX;
+        private float posY;
+        private float speed = 1.5f;
+        float scale;
+
+        public DrawableObject(Texture2D t, float x, float y, float s)
+        {
+            texture = t;
+            posX = x;
+            posY = y;
+            scale = s;
+        }
+
+        public Vector2 GetPosition() { return new Vector2(posX, posY); }
+        public Vector2 GetScaleVector() { return new Vector2(scale, scale); }
+        public Texture2D GetTexture() { return texture; }
+
+        public bool CheckForColision(DrawableObject obj)
+        {
+            bool areColliding = false;
+
+            if (posX < (obj.posX + (obj.texture.Width * obj.scale)) && (posX + (texture.Width * scale)) > obj.posX &&
+                posY < (obj.posY + (obj.texture.Height * obj.scale)) && (posY + (texture.Height * scale)) > obj.posY)
+                areColliding = true;
+
+            if (!areColliding)
+            {
+                if (obj.posX < (posX + (texture.Width * scale)) && (obj.posX + (obj.texture.Width * obj.scale)) > posX &&
+                obj.posY < (posY + (texture.Height * scale)) && (obj.posY + (obj.texture.Height * obj.scale)) > posY)
+                    areColliding = true;
+            }
+
+
+            return areColliding;
+        }
+
+        public void AddForce(float forceX, float forceY)
+        {
+            posX += forceX * speed;
+            posY += forceY * speed;
+        }
+    }
+}
