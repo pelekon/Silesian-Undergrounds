585bfdb096a0ef195c14c4eec3d30c2ab35c2b62
Paxon96 <bwloczyk@gmail.com>
2019-03-22 17:11:49 +0100

    Modify GameObject and add collision

--- Silesian Undergrounds/Silesian Undergrounds/Engine/Common/Gameobject.cs
+++ Silesian Undergrounds/Silesian Undergrounds/Engine/Common/Gameobject.cs
@@ -5,15 +5,29 @@ namespace Silesian_Undergrounds.Engine.Common
 {
     class Gameobject
     {
-        Texture2D texure;
-        Vector2 position;
+        public Texture2D texture;
+        protected Vector2 position;
         float rotation;
         float speed;
+        public Vector2 size;
 
-        void AddForce(float forceX, float forceY)
+        public Rectangle Rectangle
+        {
+            get
+            {
+                return new Rectangle((int)position.X, (int)position.Y, (int)size.X, (int)size.Y);
+            }
+        }
+
+        protected void AddForce(float forceX, float forceY)
         {
             position.X += forceX;
             position.Y += forceY;
+        }
+
+        public void NotifyCollision(Gameobject gameobject)
+        {
+
         }
     }
 }
--- Silesian Undergrounds/Silesian Undergrounds/Engine/Player/Player.cs
+++ Silesian Undergrounds/Silesian Undergrounds/Engine/Player/Player.cs
@@ -3,10 +3,85 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Silesian_Undergrounds.Engine.Common;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace Silesian_Undergrounds.Engine.Player
 {
-    class Player
+    class Player : Gameobject
     {
+        public Player(Texture2D texture)
+        {
+            this.texture = texture;
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+            Move();
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture: texture, destinationRectangle: Rectangle);
+        }
+
+        public void Collision(List<Gameobject> gameobjects)
+        {
+            foreach(Gameobject gameobject in gameobjects)
+            {
+                if(TouchingBottom(gameobject) || TouchingLeftSide(gameobject) || TouchingRightSide(gameobject) || TouchingTop(gameobject))
+                {
+                    gameobject.NotifyCollision(this);
+                }
+            }
+        }
+
+        private void Move()
+        {
+            KeyboardState state = Keyboard.GetState();
+
+            if (state.IsKeyDown(Keys.Left))
+                AddForce(-1, 0);
+            else if (state.IsKeyDown(Keys.Right))
+                AddForce(1, 0);
+            if (state.IsKeyDown(Keys.Up))
+                AddForce(0, -1);
+            else if (state.IsKeyDown(Keys.Down))
+                AddForce(0, 1);
+        }
+
+        private bool TouchingLeftSide(Gameobject gameobjects)
+        {
+            return this.Rectangle.Right > gameobjects.Rectangle.Left &&
+              this.Rectangle.Left < gameobjects.Rectangle.Left &&
+              this.Rectangle.Bottom > gameobjects.Rectangle.Top &&
+              this.Rectangle.Top < gameobjects.Rectangle.Bottom;
+        }
+
+        private bool TouchingRightSide(Gameobject gameobjects)
+        {
+            return this.Rectangle.Left < gameobjects.Rectangle.Right &&
+              this.Rectangle.Right > gameobjects.Rectangle.Right &&
+              this.Rectangle.Bottom > gameobjects.Rectangle.Top &&
+              this.Rectangle.Top < gameobjects.Rectangle.Bottom;
+        }
+
+        private bool TouchingTop(Gameobject gameobjects)
+        {
+            return this.Rectangle.Bottom > gameobjects.Rectangle.Top &&
+              this.Rectangle.Top < gameobjects.Rectangle.Top &&
+              this.Rectangle.Right > gameobjects.Rectangle.Left &&
+              this.Rectangle.Left < gameobjects.Rectangle.Right;
+        }
+
+        private bool TouchingBottom(Gameobject gameobjects)
+        {
+            return this.Rectangle.Top < gameobjects.Rectangle.Bottom &&
+              this.Rectangle.Bottom > gameobjects.Rectangle.Bottom &&
+              this.Rectangle.Right > gameobjects.Rectangle.Left &&
+              this.Rectangle.Left < gameobjects.Rectangle.Right;
+        }
     }
 }
