8d0eb636b8085820ae2d7e24dcf8e7a48965768d
Pelekon <pelekon@gmail.com>
2019-03-22 16:32:43 +0100

    Add empty file for scene.

---
+++ Silesian Undergrounds/Silesian Undergrounds/Engine/Scene/Scene.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Silesian_Undergrounds.Engine.Scene
+{
+    class Scene
+    {
+        // TODO: add code based on prepared design of class
+    }
+}
