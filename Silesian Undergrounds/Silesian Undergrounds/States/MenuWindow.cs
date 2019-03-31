99c5293c3e16b58bbb71d6a09ad5c15dac0ff26e
TomKo1 <kottomasz98@gmail.com>
2019-03-31 17:20:18 +0200

    Added basic states structure

---
+++ Silesian Undergrounds/Silesian Undergrounds/States/MenuWindow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Silesian_Undergrounds.States
+{
+    class MenuWindow
+    {
+
+        private Texture2D ButtonTexture;
+        private Texture2D BackgroundTexture;
+
+        public MenuWindow(Game1 game, GraphicsDevice graphicDevice, Microsoft.Xna.Framework.Content.ContentManager content) 
+        {
+            ButtonTexture = content.Load<Texture2D>("asdasd");
+            BackgroundTexture = content.Load<Texture2D>("asdasd");
+        }
+
+    }
+}
