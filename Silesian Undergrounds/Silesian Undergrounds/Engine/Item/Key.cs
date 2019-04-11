3419d3a7a0188ee4747b41ce0119a49bff2beb13
Pelekon <pelekon@gmail.com>
2019-04-11 15:29:53 +0200

    Add colliders to pickable items.

--- Silesian Undergrounds/Silesian Undergrounds/Engine/Item/Key.cs
+++ Silesian Undergrounds/Silesian Undergrounds/Engine/Item/Key.cs
@@ -9,7 +9,7 @@ namespace Silesian_Undergrounds.Engine.Item {
 
         public Key(Texture2D texture, Vector2 position, Vector2 size, int layer, Scene.Scene scene) : base(texture, position, size, layer, scene)
         {
-            BoxCollider collider = new BoxCollider(this, 40, 40, 0, 0, true);
+            BoxCollider collider = new BoxCollider(this, 35, 45, 0, -4, true);
             AddComponent(collider);
         }
 
