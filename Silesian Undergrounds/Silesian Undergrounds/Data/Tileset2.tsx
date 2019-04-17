23517cb96e5102cc714c7ecdceed1781df174655
209356 <kamilklyta341@gmail.com>
2019-04-17 17:27:08 +0200

    Add left and right assets for entry

--- Silesian Undergrounds/Silesian Undergrounds/Data/Tileset2.tsx
+++ Silesian Undergrounds/Silesian Undergrounds/Data/Tileset2.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.3" name="Tileset" tilewidth="64" tileheight="64" tilecount="11" columns="0">
+<tileset version="1.2" tiledversion="2019.04.03" name="Tileset" tilewidth="64" tileheight="64" tilecount="15" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="64" height="64" source="../Content/test.png"/>
@@ -33,5 +33,17 @@
  </tile>
  <tile id="10">
   <image width="64" height="64" source="../Content/top.png"/>
+ </tile>
+ <tile id="11">
+  <image width="64" height="64" source="../Content/left-entry-bottom.png"/>
+ </tile>
+ <tile id="12">
+  <image width="64" height="64" source="../Content/left-entry-top.png"/>
+ </tile>
+ <tile id="13">
+  <image width="64" height="64" source="../Content/right-entry-bottom.png"/>
+ </tile>
+ <tile id="14">
+  <image width="64" height="64" source="../Content/right-entry-top.png"/>
  </tile>
 </tileset>
