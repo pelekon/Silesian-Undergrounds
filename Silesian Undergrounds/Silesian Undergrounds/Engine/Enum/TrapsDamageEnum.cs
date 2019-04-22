ba6aa01144f691e185288c92734df823b619051b
TomKo1 <kottomasz98@gmail.com>
2019-04-22 14:35:37 +0200

    Added spike class and traps enum

---
+++ Silesian Undergrounds/Silesian Undergrounds/Engine/Enum/TrapsDamageEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Silesian_Undergrounds.Engine.Enum
+{
+    public enum TrapsDamageEnum
+    { 
+            Spikes = 20
+    }
+}
---
+++ Silesian Undergrounds/Silesian Undergrounds/Engine/Traps/Spike.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Silesian_Undergrounds.Engine.Collisions;
+using Silesian_Undergrounds.Engine.Common;
+using Silesian_Undergrounds.Engine.CommonF;
+using Silesian_Undergrounds.Engine.Enum;
+using Silesian_Undergrounds.Engine.Utils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Silesian_Undergrounds.Engine.Traps
+{
+    public class Spike : PickableItem
+    {
+        public Spike(Texture2D texture, Vector2 position, Vector2 size, int layer, Scene.Scene scene) : base(texture, position, size, layer, scene, isBuyable: false)
+        {
+            TextureMgr.Instance.LoadIfNeeded("Items/Traps/temporary_spike");
+
+            BoxCollider collider = new BoxCollider(this, 59, 46, 0, 0, false);
+            AddComponent(collider);
+        }
+
+
+        public override void NotifyCollision(GameObject obj)
+        {
+            base.NotifyCollision(obj);
+
+            if (obj is Player)
+                // add damage
+                ((Player)obj).DecreaseLiveValue((int)TrapsDamageEnum.Spikes);
+        }
+
+    }
+}
