3decf882275298a93e93d4b606a5dc1f64462965
Paxon96 <bwloczyk@gmail.com>
2019-04-24 17:00:02 +0200

    Add player statistic

---
+++ Silesian Undergrounds/Silesian Undergrounds/Engine/Common/PlayerStatistic.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Silesian_Undergrounds.Engine.Common {
+    class PlayerStatistic : StatisticHolder {
+
+        private int moneyAmount;
+        private int keyAmount;
+        private int hungerValue;
+   
+
+        public PlayerStatistic(int health, int maxHealth, float movementSpeed, float attackSpeed, int baseDamage, int moneyAmount, int keyAmount, int hungerValue) : base(health, maxHealth, movementSpeed, attackSpeed, baseDamage)
+        {
+            this.moneyAmount = moneyAmount;
+            this.keyAmount = keyAmount;
+            this.hungerValue = hungerValue;
+        }
+
+
+
+    }
+}
