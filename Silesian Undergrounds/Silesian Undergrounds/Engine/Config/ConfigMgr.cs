ed77ef69d0f14e62738ec953ae62fd8a2b59d2d6
Kamil Klyta <kamilklyta341@gmail.com>
2019-05-17 15:43:50 +0200

    Add config manager for loading config file and constants class file

---
+++ Silesian Undergrounds/Silesian Undergrounds/Engine/Config/ConfigMgr.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using Microsoft.Xna.Framework;
+using Newtonsoft.Json;
+using Silesian_Undergrounds.Engine.Common;
+using Silesian_Undergrounds.Engine.Scene;
+
+namespace Silesian_Undergrounds.Engine.Config
+{
+    public class ConfigMgr
+    {
+
+        private ConfigMgr() {}
+        private static Config _config;
+        public static Config Config => _config;
+        public static PlayerConfig PlayerConfig => _config.Player;
+        public static ChestConfig ChestConfig => _config.Chest;
+        public static PickableConfig PickableConfig => _config.Pickable;
+        public static HeartConfig HeartConfig => _config.Pickable.Heart;
+
+        public static Config LoadConfig()
+        {
+            _config = ReadConfigFile();
+            return _config;
+        }
+
+        private static Config ReadConfigFile()
+        {
+            var configFilePath = Path.Combine(Constants.DataDirectory, Constants.ConfigFileName);
+            var fileExists = File.Exists(configFilePath);
+            if (!fileExists) return null;
+           Config configFile;
+            using (var file = File.OpenText(configFilePath))
+            {
+                try { configFile = (Config)new JsonSerializer().Deserialize(file, typeof(Config)); }
+                catch (Newtonsoft.Json.JsonSerializationException e)
+                {
+                    #if DEBUG
+                        Console.WriteLine(e.Message);
+                    #endif
+                    return null;
+                }
+            }
+            Console.WriteLine("PlayerSpeed: " + configFile.Player.PlayerSpeed);
+            return configFile;
+        }
+    }
+}
---
+++ Silesian Undergrounds/Silesian Undergrounds/Engine/Config/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace Silesian_Undergrounds.Engine.Config
+{
+    public class Constants
+    {
+        public const string 
+            JsonFileExtension = ".json", 
+            DataDirectory = "Data",
+            ConfigFileName = "config.json";
+    }
+}
