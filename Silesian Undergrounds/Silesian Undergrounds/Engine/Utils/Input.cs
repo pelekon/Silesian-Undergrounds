a8136adac698dffa86986d6a6c670792bcf1fa75
Paxon96 <bwloczyk@gmail.com>
2019-06-18 16:56:52 +0200

    Fix menu scene

---
+++ Silesian Undergrounds/Silesian Undergrounds/Engine/Utils/Input.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+
+namespace Silesian_Undergrounds.Engine.Utils {
+    public static class Input {
+
+        private static KeyboardState keyboardState = Keyboard.GetState();
+        private static KeyboardState lastKeyboardState;
+
+
+        public static void Update()
+        {
+            lastKeyboardState = keyboardState;
+            keyboardState = Keyboard.GetState();
+        }
+
+        public static bool IsKeyDown(Keys input)
+        {
+            return keyboardState.IsKeyDown(input);
+        }
+
+        public static bool IsKeyUp(Keys input)
+        {
+            return keyboardState.IsKeyUp(input);
+        }
+
+        public static bool KeyPressed(Keys input)
+        {
+            if (keyboardState.IsKeyDown(input) == true && lastKeyboardState.IsKeyDown(input) == false)
+                return true;
+            else
+                return false;
+        }
+    }
+}
