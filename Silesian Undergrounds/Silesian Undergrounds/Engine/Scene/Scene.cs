65f86544534f77ce6b810401358dc5fefc2cbf69
Kamil Klyta <kamilklyta341@gmail.com>
2019-07-07 19:18:23 +0200

    Add visibility radius shader

--- Silesian Undergrounds/Silesian Undergrounds/Engine/Scene/Scene.cs
+++ Silesian Undergrounds/Silesian Undergrounds/Engine/Scene/Scene.cs
@@ -14,294 +14,288 @@ using Silesian_Undergrounds.Engine.UI.Controls;
 
 namespace Silesian_Undergrounds.Engine.Scene
 {
-    public class Scene
+  public class Scene
+  {
+
+    #region SCENE_VARIABLES
+    private List<GameObject> gameObjects;
+    private List<GameObject> objectsToDelete;
+    private List<GameObject> objectsToAdd;
+    private List<GameObject> transitions;
+
+    public Player player;
+    private UIArea ui;
+    private UIArea pauseMenu;
+    public Camera camera { get; private set; }
+    private Func<bool> OnPlayerWin;
+
+    public bool isPaused { get; private set; }
+    public bool isEnd { get; private set; }
+    public bool lastScene { get; private set; }
+    private bool isBoosterPicked;
+    private const float shaderDelayInSeconds = 5;
+    private float remainingShaderDelayInSeconds = shaderDelayInSeconds;
+    private readonly bool canUnPause;
+
+    #endregion
+
+    public Scene(PlayerStatistic playerStatistic)
     {
+      CollisionSystem.CleanUp();
 
-        #region SCENE_VARIABLES
-        private List<GameObject> gameObjects;
-        private List<GameObject> objectsToDelete;
-        private List<GameObject> objectsToAdd;
-        private List<GameObject> transitions;
+      InitLists();
+      isPaused = false;
+      player = new Player(new Vector2(200, 200), new Vector2(ResolutionMgr.TileSize, ResolutionMgr.TileSize), 1, new Vector2(2.5f, 2.5f), playerStatistic);
 
-        public Player player;
-        private UIArea ui;
-        private UIArea pauseMenu;
-        public Camera camera { get; private set; }
-        private Func<bool> OnPlayerWin;
+      TextureMgr.Instance.LoadIfNeeded("minerCharacter");
+      player.texture = TextureMgr.Instance.GetTexture("minerCharacter");
+      player.Initialize();
+      gameObjects.Add(player);
+      this.lastScene = lastScene;
+      camera = new Camera(player);
+      ui = new InGameUI(player);
+      pauseMenu = CreatePauseMenu();
+      canUnPause = true;
+    }
 
-        public bool isPaused { get; private set; }
-        public bool isEnd { get; private set; }
-        public bool lastScene { get; private set; }
-        private bool isBoosterPicked;
-        private const float shaderDelayInSeconds = 5;
-        private float remainingShaderDelayInSeconds = shaderDelayInSeconds;
-        private readonly bool canUnPause;
+    public bool PlayerPickedBooster()
+    {
+      this.isBoosterPicked = true;
+      return true;
+    }
 
-        #endregion
+    private PauseView CreatePauseMenu()
+    {
+      PauseView pauseView = new PauseView();
+      pauseView.GetResumeButton().SetOnClick(ResumeGame);
+      return pauseView;
+    }
+    public Scene(UIArea area, bool setSceneIsEnd = false)
+    {
+      isEnd = setSceneIsEnd;
+      pauseMenu = area;
+      isPaused = true;
+      canUnPause = false;
+      camera = new Camera(null);
+      InitLists();
+    }
 
-        public Scene(PlayerStatistic playerStatistic)
+    void InitLists()
+    {
+      gameObjects = new List<GameObject>();
+      objectsToDelete = new List<GameObject>();
+      objectsToAdd = new List<GameObject>();
+      transitions = new List<GameObject>();
+    }
+    #region SCENE_OBJECTS_MANAGMENT_METHODS
+
+    public void SetOnWin(Func<bool> functionOnWin)
+    {
+      this.OnPlayerWin += functionOnWin;
+    }
+
+    public void DecreaseHungerDropInterval()
+    {
+      this.player.ChangerHungerDecreaseIntervalBy(ConfigMgr.PlayerConfig.HungerDecreaseIntervalChangedByPercent);
+    }
+
+    public void SetEndGameButtonInPauseMenu(Func<bool> functionOnExitGame)
+    {
+      PauseView pV = (PauseView)this.pauseMenu;
+      pV.GetEndGameButton().SetOnClick(functionOnExitGame);
+      this.pauseMenu = pV;
+    }
+
+    public bool ResumeGame()
+    {
+      this.isPaused = false;
+      return true;
+    }
+
+    public void SetLastScene(bool isLastScene)
+    {
+      this.lastScene = isLastScene;
+    }
+
+    public void AddTransition(GameObject obj)
+    {
+      transitions.Add(obj);
+      objectsToAdd.Add(obj);
+    }
+
+    public void AddObject(GameObject obj)
+    {
+      objectsToAdd.Add(obj);
+    }
+
+    public void DeleteObject(GameObject obj)
+    {
+      objectsToDelete.Add(obj);
+    }
+
+    private void AddObjects()
+    {
+      foreach (var obj in objectsToAdd)
+        gameObjects.Add(obj);
+
+      objectsToAdd.Clear();
+    }
+
+    private void DeleteObjects()
+    {
+      foreach (var obj in objectsToDelete)
+      {
+        obj.RemoveAllComponents();
+        gameObjects.Remove(obj);
+      }
+
+      objectsToDelete.Clear();
+    }
+
+    public List<GameObject> GameObjects
+    {
+      get { return gameObjects; }
+    }
+
+    #endregion
+
+    public void Update(GameTime gameTime)
+    {
+      if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Input.KeyPressed(Keys.Escape))
+      {
+        if (isPaused && canUnPause)
+          isPaused = false;
+        else
+          isPaused = true;
+      }
+
+      if (isPaused)
+      {
+        pauseMenu.Update(gameTime);
+        return;
+      }
+
+      var timer = (float)gameTime.ElapsedGameTime.TotalSeconds;
+      remainingShaderDelayInSeconds -= timer;
+
+      if (remainingShaderDelayInSeconds <= 0)
+      {
+        remainingShaderDelayInSeconds = shaderDelayInSeconds;
+        isBoosterPicked = false;
+      }
+
+      // Operation of add or remove from gameObjects list has to appear before updating gameObjects
+      AddObjects();
+      DeleteObjects();
+
+      foreach (var obj in gameObjects)
+        obj.Update(gameTime);
+
+      camera.Update(gameTime);
+
+      ui.Update(gameTime);
+
+      DetectPlayerOnTransition();
+    }
+
+    public void Draw()
+    {
+      if (player != null)
+      {
+        Drawer.Shaders.DrawVisibilityRadiusShader((spriteBatch, gameTime) =>
         {
-            CollisionSystem.CleanUp();
+          foreach (var obj in gameObjects)
+          {
+            if (obj is Player)
+              continue;
 
-            InitLists();
-            isPaused = false;
-            player = new Player(new Vector2(200, 200), new Vector2(ResolutionMgr.TileSize, ResolutionMgr.TileSize), 1, new Vector2(2.5f, 2.5f), playerStatistic);
+            obj.Draw(spriteBatch);
+          }
+        }, transformMatrix: camera.Transform, lightSource: player.position);
+      }
 
-            TextureMgr.Instance.LoadIfNeeded("minerCharacter");
-            player.texture = TextureMgr.Instance.GetTexture("minerCharacter");
-            player.Initialize();
-            gameObjects.Add(player);
-            this.lastScene = lastScene;
-            camera = new Camera(player);
-            ui = new InGameUI(player);
-            pauseMenu = CreatePauseMenu();
-            canUnPause = true;
+      if (player != null)
+      {
+        Drawer.Shaders.DrawShadowEffect((spriteBatch, gameTime) =>
+        {
+          foreach (var obj in gameObjects)
+            if (obj.layer == 3)
+              obj.Draw(spriteBatch);
+        }, transformMatrix: camera.Transform, lightSource: player.position);
+      }
+      if (player != null)
+      {
+
+        Drawer.Shaders.DrawVisibilityRadiusShader((spriteBatch, gameTime) =>
+        {
+          if (player != null)
+            player.Draw(spriteBatch);
+
+          foreach (var obj in gameObjects)
+          {
+            if (obj.layer == 6)
+              obj.Draw(spriteBatch);
+          }
+
+        }, transformMatrix: camera.Transform, lightSource: player.position);
+      }
+
+      Drawer.Shaders.DrawFoggEffect((spriteBatch, gameTime) =>
+      {
+        if (pauseMenu != null && pauseMenu.BackgroundImage != null)
+        {
+                // draw fogg shader
+                Image bgImage = pauseMenu.BackgroundImage;
+          bgImage.Draw(spriteBatch);
         }
+      }, transformMatrix: camera.Transform);
 
-        public bool PlayerPickedBooster()
+      Drawer.Draw((spriteBatch, gameTime) =>
+      {
+        if (isPaused)
+          pauseMenu.Draw(spriteBatch);
+        else
+          ui.Draw(spriteBatch);
+      }, null);
+
+      if (isBoosterPicked && player != null)
+      {
+        Drawer.Shaders.DrawBoosterPickupShader((spriteBatch, gameTime) =>
         {
-            this.isBoosterPicked = true;
-            return true;
-        }
+          player.Draw(spriteBatch);
+        }, transformMatrix: camera.Transform);
+      }
 
-        private PauseView CreatePauseMenu()
+      Drawer.Shaders.DrawShopNeonShader((spritebatch, gametime) =>
+      {
+        foreach (var obj in gameObjects)
+          if (obj.layer == (int)LayerEnum.ShopPickables)
+            obj.Draw(spritebatch);
+      }, transformMatrix: camera.Transform);
+    }
+
+    private void DetectPlayerOnTransition()
+    {
+      foreach (var transition in transitions)
+        if (player.GetTileWhereStanding() == transition.GetTileWhereStanding())
         {
-            PauseView pauseView = new PauseView();
-            pauseView.GetResumeButton().SetOnClick(ResumeGame);
-            return pauseView;
-        }
-        public Scene(UIArea area, bool setSceneIsEnd = false)
-        {
-            isEnd = setSceneIsEnd;
-            pauseMenu = area;
-            isPaused = true;
-            canUnPause = false;
-            camera = new Camera(null);
-            InitLists();
-        }
+          foreach (var obj in gameObjects)
+            DeleteObject(obj);
 
-        void InitLists()
-        {
-            gameObjects = new List<GameObject>();
-            objectsToDelete = new List<GameObject>();
-            objectsToAdd = new List<GameObject>();
-            transitions = new List<GameObject>();
-        }
-        #region SCENE_OBJECTS_MANAGMENT_METHODS
+          foreach (var obj in transitions)
+            DeleteObject(obj);
 
-        public void SetOnWin(Func<bool> functionOnWin)
-        {
-            this.OnPlayerWin += functionOnWin;
-        }
+          foreach (var obj in objectsToAdd)
+            DeleteObject(obj);
 
-        public void DecreaseHungerDropInterval()
-        {
-            this.player.ChangerHungerDecreaseIntervalBy(ConfigMgr.PlayerConfig.HungerDecreaseIntervalChangedByPercent);
-        }
-
-        public void SetEndGameButtonInPauseMenu(Func<bool> functionOnExitGame)
-        {
-            PauseView pV = (PauseView)this.pauseMenu;
-            pV.GetEndGameButton().SetOnClick(functionOnExitGame);
-            this.pauseMenu = pV;
-        }
-
-        public bool ResumeGame()
-        {
-            this.isPaused = false;
-            return true;
-        }
-
-        public void SetLastScene(bool isLastScene)
-        {
-            this.lastScene = isLastScene;
-        }
-
-        public void AddTransition(GameObject obj)
-        {
-            transitions.Add(obj);
-            objectsToAdd.Add(obj);
-        }
-
-        public void AddObject(GameObject obj)
-        {
-            objectsToAdd.Add(obj);
-        }
-
-        public void DeleteObject(GameObject obj)
-        {
-            objectsToDelete.Add(obj);
-        }
-
-        private void AddObjects()
-        {
-            foreach (var obj in objectsToAdd)
-                gameObjects.Add(obj);
-
-            objectsToAdd.Clear();
-        }
-
-        private void DeleteObjects()
-        {
-            foreach (var obj in objectsToDelete)
-            {
-                obj.RemoveAllComponents();
-                gameObjects.Remove(obj);
-            }
-
-            objectsToDelete.Clear();
-        }
-
-        public List<GameObject> GameObjects
-        {
-            get { return gameObjects; }
-        }
-
-        #endregion
-
-        public void Update(GameTime gameTime)
-        {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Input.KeyPressed(Keys.Escape))
-            {
-                if (isPaused && canUnPause)
-                    isPaused = false;
-                else
-                    isPaused = true;
-            }
-
-            if (isPaused)
-            {
-                pauseMenu.Update(gameTime);
-                return;
-            }
-
-            var timer = (float)gameTime.ElapsedGameTime.TotalSeconds;
-            remainingShaderDelayInSeconds -= timer;
-
-            if (remainingShaderDelayInSeconds <= 0)
-            {
-                remainingShaderDelayInSeconds = shaderDelayInSeconds;
-                isBoosterPicked = false;
-            }
-
-            // Operation of add or remove from gameObjects list has to appear before updating gameObjects
-            AddObjects();
-            DeleteObjects();
-
-            foreach (var obj in gameObjects)
-                obj.Update(gameTime);
-
-            camera.Update(gameTime);
-
-            ui.Update(gameTime);
-
-            DetectPlayerOnTransition();
-        }
-
-        public void Draw()
-        {
-            Drawer.Shaders.DrawGrayScaleEffect((spriteBatch, gameTime) =>
-            {
-                foreach (var obj in gameObjects)
-                {
-                    if (obj is Player)
-                        continue;
-
-                    if (obj.layer != 3)
-                        obj.Draw(spriteBatch);
-                }
-            }, transformMatrix: camera.Transform);
-
-            Drawer.Draw((spriteBatch, gameTime) =>
-            {
-                foreach (var obj in gameObjects)
-                {
-                    if (obj is Player)
-                        continue;
-
-                    obj.Draw(spriteBatch);
-                }
-            }, transformMatrix: camera.Transform);
-
-            if (player != null)
-            {
-                Drawer.Shaders.DrawShadowEffect((spriteBatch, gameTime) =>
-                {
-                    foreach (var obj in gameObjects)
-                        if (obj.layer == 3)
-                            obj.Draw(spriteBatch);
-                }, transformMatrix: camera.Transform, lightSource: player.position);
-            }
-
-            Drawer.Draw((spriteBatch, gameTime) =>
-            {
-                if (player != null)
-                    player.Draw(spriteBatch);
-
-                foreach (var obj in gameObjects)
-                {
-                    if (obj.layer == 6)
-                        obj.Draw(spriteBatch);
-                }
-
-            }, transformMatrix: camera.Transform);
-
-            Drawer.Shaders.DrawFoggEffect((spriteBatch, gameTime) =>
-            {
-                if (pauseMenu != null && pauseMenu.BackgroundImage != null)
-                {
-                    // draw fogg shader
-                    Image bgImage = pauseMenu.BackgroundImage;
-                    bgImage.Draw(spriteBatch);
-                }
-            }, transformMatrix: camera.Transform);
-
-            Drawer.Draw((spriteBatch, gameTime) =>
-            {
-                if (isPaused)
-                    pauseMenu.Draw(spriteBatch);
-                else
-                    ui.Draw(spriteBatch);
-            }, null);
-
-            if (isBoosterPicked && player != null)
-            {
-                Drawer.Shaders.DrawBoosterPickupShader((spriteBatch, gameTime) =>
-                {
-                    player.Draw(spriteBatch);
-                }, transformMatrix: camera.Transform);
-            }
-
-            Drawer.Shaders.DrawShopNeonShader((spritebatch, gametime) =>
-            {
-               foreach (var obj in gameObjects)
-                  if (obj.layer == (int)LayerEnum.ShopPickables)
-                       obj.Draw(spritebatch);
-            }, transformMatrix: camera.Transform);
-        }
-
-        private void DetectPlayerOnTransition()
-        {
-            foreach (var transition in transitions)
-                if (player.GetTileWhereStanding() == transition.GetTileWhereStanding())
-                {
-                    foreach (var obj in gameObjects)
-                        DeleteObject(obj);
-
-                    foreach (var obj in transitions)
-                        DeleteObject(obj);
-
-                    foreach (var obj in objectsToAdd)
-                        DeleteObject(obj);
-
-                    DeleteObjects();
-                    isEnd = true;
-                    if (lastScene)
-                    {
-                        OnPlayerWin.Invoke();
-                    }
-                }
+          DeleteObjects();
+          isEnd = true;
+          if (lastScene)
+          {
+            OnPlayerWin.Invoke();
+          }
         }
     }
+  }
 }
 
